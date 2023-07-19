61d8412665acdcc4d75de837b27d435265e9cc50
bavichugo <victorhugobatistela@gmail.com>
2023-07-18 22:43:48 -0700

    Add auth firebase scripts

---
+++ src/firebase/firebase.js
@@ -0,0 +1,25 @@
+// Initial firebase setup
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
+// Get your config from Firebase Console
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
