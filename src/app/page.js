37424908f1d7416c0af21d1eb71f9b009e9a2186
bavichugo <victorhugobatistela@gmail.com>
2023-07-24 21:40:47 -0700

    Remove unnecessar codey

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      <h1 className="text-white text-9xl mx-auto">CONTENT</h1>
-      <h1 className="text-white text-9xl mx-auto">CONTENT</h1>
-      
-      
+      <h1 className="text-white mx-auto">CONTENT</h1>
     </div>
   )
 }
--- src/components/NavBar/NavBar1.js
+++ src/components/NavBar/NavBar1.js
@@ -2,8 +2,9 @@
 import Link from "next/link";
 import logo from "../../assets/logo.png";
 import Image from "next/image";
+import { FiMenu } from "react-icons/fi";
 import { useState } from "react";
-import { usePathname } from "next/navigation"
+import { usePathname } from "next/navigation";
 
 const NAV_ITEMS = [
   {
@@ -45,29 +46,11 @@ const Navbar1 = () => {
           </span>
         </Link>
         <button
-          data-collapse-toggle="navbar-default"
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded={`${isExpanded}`}
           onClick={() => toggleExpansion(!isExpanded)}
         >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
+          <FiMenu className="text-2xl" />
         </button>
         <div
           className={`${
@@ -75,10 +58,15 @@ const Navbar1 = () => {
           } w-full delay-1000 transition-transform ease-linear md:block md:w-auto`}
           id="navbar-default"
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 borde rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
+          <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4 borde rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
             {NAV_ITEMS.map((item) => {
               return (
-                <NavLink key={item.link} name={item.name} link={item.link} pathname={pathname}/>
+                <NavLink
+                  key={item.link}
+                  name={item.name}
+                  link={item.link}
+                  pathname={pathname}
+                />
               );
             })}
           </ul>
@@ -93,7 +81,9 @@ const NavLink = ({ name, link, pathname }) => {
     <li>
       <Link
         href={link}
-        className={`block py-2 pl-3 pr-4 hover:bg-white md:hover:bg-transparent hover:text-blue-600 rounded md:bg-transparent md:p-0 ${pathname === link ? "text-blue-600" : "text-white"}`}
+        className={`block py-2 pl-3 pr-4 hover:bg-white md:hover:bg-transparent hover:text-blue-600 rounded md:bg-transparent md:p-0 ${
+          pathname === link ? "text-blue-600" : "text-white"
+        }`}
       >
         {name}
       </Link>
